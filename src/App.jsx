7f141977fd9bde877421ab9fdd8e72d21f82d53c
Evan Bishop <evan.bishop888@gmail.com>
2017-08-27 18:54:08 -0700

    add code in Clock.jsx that calculates and displays time until chosen date

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
   }
 
   changeDeadline(){
-    this.setState({deadline: 'August 29, 2017'})
+    // this.setState({deadline: 'August 29, 2017'})
     // Never mutate or change state directly
+    // console.log('state', this.state);
+    this.setState({deadline: this.state.newDeadline})
   }
 
   render() {
@@ -20,14 +24,14 @@ class App extends Component {
         <div className="App-title">
           Countdown to {this.state.deadline}
         </div>
+        <Clock
+          deadline={this.state.deadline}
+        />
         <div>
-          <div className='Clock-days'>Days</div>
-          <div className='Clock-hours'>Hours</div>
-          <div className='Clock-minutes'>Minutes</div>
-          <div className='Clock-seconds'>Seconds</div>
-        </div>
-        <div>
-          <input placeholder='new date'/>
+          <input
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
           <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
 
